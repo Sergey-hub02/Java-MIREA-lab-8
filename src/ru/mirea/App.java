f661a79440ccb035a5304c61c761071cd06f5582
Sergey-hub02 <s.park190802@gmail.com>
2021-09-19 13:47:55 +0300

    Работа выполнена

--- src/ru/mirea/App.java
+++ src/ru/mirea/App.java
@@ -1,4 +1,22 @@
 package ru.mirea;
 
+import java.util.Scanner;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class App {
+  private static final Scanner IN = new Scanner(System.in);
+  private static final String FILE = "/home/ezhik/Documents/Java-MIREA/labs/Java-MIREA-lab-8/out.txt";
+
+  public static void main(String[] args) throws IOException {
+    System.out.print("Что записать в файл: ");
+    String line = IN.nextLine();
+
+    FileWriter fw = new FileWriter(new File(FILE));
+    fw.write(line);
+
+    fw.close();
+  }
 }
